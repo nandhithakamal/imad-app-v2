30c64ffd666127f169cbd00c14a945ba537a1068
nandhithakamal <nandhithakml@gmail.com>
2017-03-30 15:56:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,28 @@
-
+var submit = getELementById("submitButton");
+submit.onclick = function(){
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            if(request.state === 200){
+                console.log("User " + username + " logged in!");
+                alert("Logged in successfully as " + username);
+                
+            }else if(request.state === 483){
+                alert("Incorrect username/password!");
+                
+            }else if(request.state === 500){
+                alert("Something went wrong with the server and we do not know what it is. :( ");
+                
+            }
+        }
+    };
+    
+    var username = document.getELementById('username').value;
+    var password = document.getELementById('password').value;
+    request.open('POST', 'http://nandhithakamal.imad.hasura-app.io/login', true);
+    request.send(JSON.stringify({username: username, password: password}));
+    
+    
+};
